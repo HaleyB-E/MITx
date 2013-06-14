c5be7c310fc57f43858d4eb1ea40bb2597186fe8
Haley B-E <hbrandterichsen@gmail.com>
2013-06-14 16:31:05 -0400

    more work on graphingcalculator

--- graphingcalculator.js
+++ graphingcalculator.js
@@ -1,33 +1,30 @@
+// Graphing Calculator javascript
+
 
 $(document).ready(function(){
-    $('.graphcalc').each(function(){
+    $('.calculator').each(function(){
         setup_calc(this);
     });
 });
 
+
 function setup_calc(div) {
     
-    //sets up graph
+    //sets up graph display and graphing parameters
+    var screenDiv = $('<div></div>');
+    screenDiv.append('<canvas width="400" height="300" class="graph">test</canvas>');
+    screenDiv.append('<text class="input-text">f(x)= </text>');
+    screenDiv.append('<input type="text" class="equation-input"></input>');
     
-    var graph = $('<canvas width="400" height="300">test</canvas>', {id: 'canvas'});
-    var input = $('<input></input>',{type: 'text', id: 'input1'});
-    var xmin = $('<input></input>',{type: 'text', id: 'input2'});
-    var xmax = $('<input></input>',{type: 'text', id: 'input2'});
-    var plot = $('<button class="smallbutton">Plot</button>');
-
-    var text0 = $('<text>f(x): </text>');
-    var text1 = $('<text>min x: </text>');
-    var text2 = $('<text>max x: </text>');
-    plot.bind("click", function(){
-        graphFunct(graph,input,xmin,xmax);
-    });
-    var graphDiv = $('<div></div>');
-    var equationDiv = $('<div></div>');
-    var xDiv = $('<div></div>');
-    var buttonDiv = $('<div></div>');
+    var plotDiv = $('<div></div>');
+    plotDiv.append('<button class="smallbutton plotbutton">Plot</button>');
+    plotDiv.append('<text class="limits">min x= </text>');
+    plotDiv.append('<input type="text" class="limits xmin"></input>');
+    plotDiv.append('<text class="limits">max x= </text>');
+    plotDiv.append('<input type="text" class="limits xmax"></input>');
     
-    //sets up calculator
-    
+   
+    //sets up remaining calculator buttons
     var row1 = $('<div></div>', {class: 'row', id: 'row1'});
     row1.append('<button class="smallbutton numbutton">x</button>');
     row1.append('<button class="smallbutton">??</button>');
@@ -36,7 +33,7 @@ function setup_calc(div) {
 
     var row2 = $('<div></div>', {class: 'row', id: 'row2'});
     row2.append('<button class="smallbutton clear">C</button>');
-    row2.append('<button class="smallbutton">&#177;</button>');
+    row2.append('<button class="smallbutton">&#177; ??</button>');
     row2.append('<button class="smallbutton mathbutton" data-operation="/">&divide;</button>');
     row2.append('<button class="smallbutton mathbutton" data-operation="*">&#215;</button>');
     
@@ -62,32 +59,30 @@ function setup_calc(div) {
     row6.append('<button class="widebutton numbutton">0</button>');
     row6.append('<button class="smallbutton decimal">.</button>');
     
+    $(div).append(screenDiv, plotDiv, row1, row2, row3, row4, row5, row6);
     
-    graphDiv.append(graph);
-    equationDiv.append(text0, input);
-    xDiv.append(text1, xmin,text2, xmax);
-    buttonDiv.append(plot);
-    
-    $(div).append(graphDiv,equationDiv,xDiv,buttonDiv, row1, row2, row3, row4, row5, row6);
     
     //sets up button behavior
+    $('.plotbutton').on("click",function (){
+        graphFunct($('.graph'), $('.equation-input'),$('.xmin'),$('.xmax'));
+    });
     $('.numbutton,.decimal').on("click",function (){
-        var current = $('#input1').val();
-        $('#input1').val(current + $(this).text());     
+        var current = $('.equation-input').val();
+        $('.equation-input').val(current + $(this).text());     
     });
     $('.mathbutton').on("click",function (){
-        var current = $('#input1').val();
-        $('#input1').val(current + $(this).data("operation"));     
+        var current = $('.equation-input').val();
+        $('.equation-input').val(current + $(this).data("operation"));     
     });
     $('.equals').on("click",function (){
-        var input = $('#input1').val();
-        $('#input1').val((String(calculate(input))));
+        var input = $('.equation-input').val();
+        $('.equation-input').val((String(calculate(input))));
     });
     $('.clear').on("click",function (){
-        $('#input1').val("");
+        $('.equation-input').val("");
     });
-    
 }
+
 
 function graphFunct(graph,input,xmin,xmax){
     var DOMgraph = graph[0];
@@ -101,39 +96,52 @@ function graphFunct(graph,input,xmin,xmax){
     
     var yvalues = [];
     
+    //tries to graph function, then displays either graph or relevant error
     try{
         var equat = calculator.parse(input.val());
-        var ymax = -1000;
-        var ymin = 1000;
+        
+        //populates yvalues with numbers, finds max and min values of y to aid scaling
+        var ymax = Number.MIN_VALUE;
+        var ymin = Number.MAX_VALUE;
         for(var x1=0; x1<=graph.width(); x1++) {
             var y1 = calculator.evaluate(equat,{'x':fromX(x1,xstart,xend,graph)});
             yvalues.push(y1);
             ymax = Math.max(ymax,y1);
             ymin = Math.min(ymin,y1);
         }
+        
+        //creates padding so graph isn't running off top/bottom of screen
         var padding = 0.1*(ymax-ymin);
         ymax += padding;
-
         ymin -= padding;
+        
+        //creates path of graph from calculated values
         for(var x2=0; x2<=graph.width(); x2++) {
             var y2 = toY(yvalues[x2],ymin,ymax,graph);
             ctx.lineTo(x2,y2);
             ctx.moveTo(x2,y2);
         }
+        
+        //draws graph
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
     }
     catch(err){
+        
+        //displays relevant error message
         ctx.fillStyle = "black";
         ctx.font = "14px sans-serif";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.fillText(err,200,200);   
     }
-    
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-    
+
+
 }
+
+
+//Functions used to scale x and y values
 
 function fromX(x,xmin,xmax, graph) {
     return x*(xmax-xmin)/graph.width()+xmin;
