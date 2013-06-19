deb8673bc575f8bd6ea6fd894f99a169ef79e604
Haley B-E <hbrandterichsen@gmail.com>
2013-06-19 09:40:12 -0400

    Added Day 8 Visual folder

---
+++ Week2/Day8Visual/visual.js
@@ -0,0 +1,11 @@
+var data= [0,4,8,8,15,16,23,42];
+
+//var chart=$("<div></div>").addClass("chart");
+//$(".chart-container").append(chart);
+
+//data.forEach(function(d) {
+//    chart.append($("<div></div>").css("width", d * 10 + "px").text(d));
+//});
+
+var chart = d3.select(".chart-container").append("div").attr("class", "chart");
+chart.selectAll("div").data(data).enter().append("div");
