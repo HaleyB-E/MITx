6242e1a6dec7bbb9af3d512170cb5c854d12af19
Haley B-E <hbrandterichsen@gmail.com>
2013-06-19 10:34:20 -0400

    created bar chart with svg and d3

--- Week2/Day8Visual/visual.js
+++ Week2/Day8Visual/visual.js
@@ -1,11 +1,26 @@
-var data= [0,4,8,8,15,16,23,42];
+var data = [0, 4, 8, 8, 15, 16, 23, 42];
+var CHART_HEIGHT = 140;
 
-//var chart=$("<div></div>").addClass("chart");
-//$(".chart-container").append(chart);
+var x_scale = d3.scale.linear().domain([0, d3.max(data)]).range(["0%", "100%"]);
+var y_scale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, CHART_HEIGHT]);
 
-//data.forEach(function(d) {
-//    chart.append($("<div></div>").css("width", d * 10 + "px").text(d));
-//});
+var chart = d3.select(".chart-container").append("svg").attr("class", "chart");
+chart.selectAll("rect").data(data)
+    .enter().append("rect")
+    .attr("width", x_scale).attr("height", 20)
+    .attr("y", function (d, i) {
+        return y_scale(i);
+    });
 
-var chart = d3.select(".chart-container").append("div").attr("class", "chart");
-chart.selectAll("div").data(data).enter().append("div");
+chart.selectAll("text").data(data)
+    .enter().append("text")
+    .attr("x", x_scale)
+    .attr("dx", -3)
+    .attr("y", function (d, i) {
+        return y_scale(i) + (y_scale.rangeBand() / 2);
+    })
+    .attr("dy", "0.35em")
+    .attr("text-anchor", "end")
+    .text(function (d) {
+        return d;
+    });
