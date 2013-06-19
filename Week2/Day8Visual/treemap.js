2d503747e03ef6dde5254ba4ae758418112a5f05
Haley B-E <hbrandterichsen@gmail.com>
2013-06-19 16:05:35 -0400

    messing around with colors some more

--- Week2/Day8Visual/treemap.js
+++ Week2/Day8Visual/treemap.js
@@ -1,4 +1,4 @@
-testData = data;
+//This doesn't do very much! but color scales are NIFTY!
 
 var OUTER_HEIGHT = 300;
 var OUTER_WIDTH = 300;
@@ -8,7 +8,8 @@ var margin = { top: 20, right: 20, bottom: 20, left: 20 };
 var chartWidth = OUTER_WIDTH - margin.left - margin.right;
 var chartHeight = OUTER_HEIGHT - margin.top - margin.bottom;
 //var chartColor = d3.scale.category20()          //COOL THING: color scales
-var chartColor = d3.scale.linear().domain([1,10]).range(['red', 'blue']);
+var chartColor1 = d3.scale.linear().domain([1,10]).range(['red', 'blue']);
+var chartColor2 = d3.scale.linear().domain([1, 10]).range(['yellow', 'red']);
 
 var treemap = d3.layout.treemap()
     .size([OUTER_WIDTH, OUTER_HEIGHT]);
@@ -19,7 +20,7 @@ var chart = d3.select(".chart-container").append("svg")
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var cell = chart.selectAll("g").data(testData)
+var cell = chart.selectAll("cell").data(data[0])
     .enter().append("g")
     .attr("class", "cell")
     .attr("transform", function(d) {
@@ -32,13 +33,21 @@ cell.append("rect")
     }).attr("height", function(d){
         return d.dy;
     }).style("fill", function(d){
-        return chartColor(d.y / 10);
+        return chartColor2(d.y / 10);
     });
 
-//
-//  cell.append("text")
-//      .attr("x", function(d) { return d.dx / 2; })
-//      .attr("y", function(d) { return d.dy / 2; })
-//      .attr("dy", ".35em")
-//      .attr("text-anchor", "middle")
-//      .text(function(d) { return d.children ? null : d.name; });
+var cell2 = chart.selectAll("cell2").data(data[1])
+    .enter().append("g")
+    .attr("class", "cell2")
+    .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
+
+cell2.append("rect")
+    .attr("width", function(d) {
+        return d.dx;
+    }).attr("height", function(d){
+        return d.dy;
+    }).style("fill", function(d){
+        return chartColor1((d.y - 100) / 10);
+    });
