914cd7117d24058d1730cf36a5e6c673e23ec7f4
Haley B-E <hbrandterichsen@gmail.com>
2013-06-20 15:52:15 -0400

    added updating scoreboard

--- Week2/Knapsack/knapsack.js
+++ Week2/Knapsack/knapsack.js
@@ -2,12 +2,22 @@ var knapsack = (function() {
 
     var currentWeight = 0;
     var maxWeight = 0;
+    var currentValue = 0;
     
     function setup(div){ 
         
-        var ungrabbedBin = $('<div class="ungrabbedBin itemBin"></div>');
-        var grabbedBin = $('<div class="grabbedBin itemBin"></div>');
+        var ungrabbedBin = $('<div class="ungrabbedBin itemBin">House</div>');
+        var grabbedBin = $('<div class="grabbedBin itemBin">Knapsack</div>');
         
+        $(grabbedBin).append('<p><text>Current value: <span class="grabbedValue">0</span></text><p><text>Current weight: <span class="grabbedWeight">0</span></text>');
+        
+        function updateDisplay(){
+            $('.grabbedValue').text(currentValue);
+            $('.grabbedWeight').text(currentWeight);
+        }
+        
+        
+        //sets initial weight to zero and max weight to page-provided limit
         currentWeight = 0;
         maxWeight = div.data("maxweight");
         
@@ -19,28 +29,34 @@ var knapsack = (function() {
         
         $(div).append(ungrabbedBin, grabbedBin);
         
-        //adds functionality to buttons so they move when clicked
+        //adds functionality to buttons so they move when clicked (if valid weightwise)
         $('.itemButton').click(function() {
-            
+            var itemWeight = $(this).find('img').data('weight');
+            var itemValue = $(this).find('img').data('value');
             if( $('.ungrabbedBin').has(this).length > 0){
-                var weightToAdd = $(this).find('img').data('weight');
-                if (validWeight(weightToAdd)){
-                    currentWeight += weightToAdd;
+                
+                if (validWeight(itemWeight)){
+                    currentWeight += itemWeight;
+                    currentValue += itemValue;
                     this.remove();
                     $('.grabbedBin').append(this);
                 }
                 else{
+                    //alert goes here
                     console.log("FAILURE");
                 }
             }
             else{
+                currentWeight -= itemWeight;
+                currentValue -= itemValue;
                 this.remove();
                 $('.ungrabbedBin').append(this);
             }
+        updateDisplay();
         });       
     }
     
-    //
+    //true indicates added weight will be over the limit
     function validWeight(weightToAdd){
         return (currentWeight + weightToAdd <= maxWeight);
         
