b34d58ffd56289232fdf0f6597eae4fe8f5d76aa
Haley B-E <hbrandterichsen@gmail.com>
2013-06-20 15:40:37 -0400

    added weight limit

--- Week2/Knapsack/knapsack.js
+++ Week2/Knapsack/knapsack.js
@@ -1,39 +1,54 @@
 var knapsack = (function() {
 
-    function setup(div){    
+    var currentWeight = 0;
+    var maxWeight = 0;
+    
+    function setup(div){ 
+        
         var ungrabbedBin = $('<div class="ungrabbedBin itemBin"></div>');
         var grabbedBin = $('<div class="grabbedBin itemBin"></div>');
         
+        currentWeight = 0;
+        maxWeight = div.data("maxweight");
+        
+        //creates buttons with each item on them and starts them in ungrabbedBin
         $('img').each(function() {
-            var button = $('<button class="itemButton"></button>').append(this);
+            var button = $('<button class="itemButton"> $' + $(this).data("value") + ',' + $(this).data("weight") + ' kg</button>').append(this);
             ungrabbedBin.append(button);     
         }); 
         
         $(div).append(ungrabbedBin, grabbedBin);
         
+        //adds functionality to buttons so they move when clicked
         $('.itemButton').click(function() {
+            
             if( $('.ungrabbedBin').has(this).length > 0){
+                var weightToAdd = $(this).find('img').data('weight');
+                if (validWeight(weightToAdd)){
+                    currentWeight += weightToAdd;
                     this.remove();
                     $('.grabbedBin').append(this);
+                }
+                else{
+                    console.log("FAILURE");
+                }
             }
             else{
-                    this.remove();
-                    $('.ungrabbedBin').append(this);
+                this.remove();
+                $('.ungrabbedBin').append(this);
             }
-        });
+        });       
+    }
+    
+    //
+    function validWeight(weightToAdd){
+        return (currentWeight + weightToAdd <= maxWeight);
         
     }
     
     return {setup: setup};
     
 })();
-
-function tester(){
-    if($('.grabbedBin').find($('.itemButton')) > 0){
-        console.log("true");
-    }
-    
-}
 
 $(document).ready(function () {
     $('.knapsack').each(function () {
